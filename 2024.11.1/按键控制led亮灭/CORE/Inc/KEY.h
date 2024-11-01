eee4fd6afbac104fc937fb1ad36099d2905cb155
luoyu008 <1796249234@qq.com>
2024-11-01 23:20:01 +0800

    2024.11.14笔记：实现hal库led点亮闪烁

---
+++ 2024.11.1/按键控制led亮灭/CORE/Inc/KEY.h
@@ -0,0 +1,6 @@
+#ifndef __KEY_H__
+#define __KEY_H__
+#include "main.h"
+uint8_t KEY_return(void);
+
+#endif
---
+++ 2024.11.1/按键控制led亮灭/CORE/Src/KEY.c
@@ -0,0 +1,11 @@
+#include "KEY.h"
+uint8_t KEY_return(void){
+	uint8_t temp;
+	if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_3)==0){
+		HAL_Delay(20);
+		while(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_3)==0);
+		HAL_Delay(20);
+		temp=1;
+	}
+	return temp;
+}
