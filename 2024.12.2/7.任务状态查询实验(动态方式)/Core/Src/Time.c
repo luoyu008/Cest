30c68fe8aca3d68e6ff185982bb8b5719b978963
luoyu008 <1796249234@qq.com>
2024-12-02 22:29:49 +0800

    2024.12.2笔记：实现任务状态查询和运行时间统计实验(动态方式)

---
+++ 2024.12.2/7.任务状态查询实验(动态方式)/Core/Src/Time.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include "OLED.h"
+TIM_HandleTypeDef Time2_FreeRtosInit;
+TIM_HandleTypeDef Time3_FreeRtosInit;
+uint32_t count2;
+uint32_t count3;
+void Time2_Init(void){
+	//ÅäÖÃ1hz
+	Time2_FreeRtosInit.Instance=TIM2;
+	Time2_FreeRtosInit.Init.Prescaler=10000-1;
+	Time2_FreeRtosInit.Init.CounterMode=TIM_COUNTERMODE_UP;
+	Time2_FreeRtosInit.Init.Period=7200-1;
+	Time2_FreeRtosInit.Init.ClockDivision=TIM_CLOCKDIVISION_DIV1;
+	HAL_TIM_Base_Init(&Time2_FreeRtosInit);
+	
+	HAL_NVIC_SetPriority(TIM2_IRQn,4,0);
+	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+	
+	
+	HAL_TIM_Base_Start_IT(&Time2_FreeRtosInit);
+	
+}
+
+void Time3_Init(void){
+	Time3_FreeRtosInit.Instance=TIM3;
+	Time3_FreeRtosInit.Init.Prescaler=10000-1;
+	Time3_FreeRtosInit.Init.CounterMode=TIM_COUNTERMODE_UP;
+	Time3_FreeRtosInit.Init.Period=7200-1;
+	Time3_FreeRtosInit.Init.ClockDivision=TIM_CLOCKDIVISION_DIV1;
+	HAL_TIM_Base_Init(&Time3_FreeRtosInit);
+	
+	HAL_NVIC_SetPriority(TIM3_IRQn,6,0);
+	HAL_NVIC_EnableIRQ(TIM3_IRQn);
+	
+	HAL_TIM_Base_Start_IT(&Time3_FreeRtosInit);
+}
+
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim){
+	if(htim->Instance==TIM2){
+		__HAL_RCC_TIM2_CLK_ENABLE();
+	}
+	if(htim->Instance==TIM3){
+		__HAL_RCC_TIM3_CLK_ENABLE();
+	}
+}
+
+void TIM2_IRQHandler(void){
+	HAL_TIM_IRQHandler(&Time2_FreeRtosInit);
+}
+
+void TIM3_IRQHandler(void){
+	HAL_TIM_IRQHandler(&Time3_FreeRtosInit);
+}
---
+++ 2024.12.2/7.任务状态查询实验(动态方式)/Core/Src/freertos.c
@@ -0,0 +1,250 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+#include "USART.h"
+#include "KEY.h"
+#include "EXIT.h"
+#include "DELAY.h"
+#include "OLED.h"
+#include <stdio.h>
+#include <stdlib.h>
+osThreadId defaultTaskHandle;
+osThreadId Task1Handle;
+osThreadId Task2Handle;
+osThreadId Task3Handle;
+
+#define Task1_STACK_SIZE 128
+#define Task1_PRIO       1
+
+#define Task2_STACK_SIZE 120
+#define Task2_PRIO       1
+
+//#define Task3_STACK_SIZE 128
+//#define Task3_PRIO       31
+
+List_t		Test_List;
+ListItem_t	Test_ListItem1;
+ListItem_t	Test_ListItem2;
+ListItem_t	Test_ListItem3;
+
+uint8_t add;
+
+void StartDefaultTask(void const * argument);
+void EntryTask1(void const * argument);
+void EntryTask2(void const * argument);
+void EntryTask3(void const * argument);
+
+void FreeRtos_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+/*
+	为空闲任务提供内存
+*/
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}
+/* USER CODE END GET_IDLE_TASK_MEMORY */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void FreeRtos_Init(void){
+	taskENTER_CRITICAL();//进入临界区
+	xTaskCreate(
+				(TaskFunction_t			) 	EntryTask1,
+				(char *					) 	"Task1",
+				(configSTACK_DEPTH_TYPE )   Task1_STACK_SIZE,
+				(void *					)	NULL,
+				(UBaseType_t			)   Task1_PRIO,
+				(TaskHandle_t*			)   &Task1Handle
+						  );
+				
+	xTaskCreate(
+				(TaskFunction_t			) 	EntryTask2,
+				(char *					) 	"Task2",
+				(configSTACK_DEPTH_TYPE )   Task2_STACK_SIZE,
+				(void *					)	NULL,
+				(UBaseType_t			)   Task2_PRIO,
+				(TaskHandle_t*			)   &Task2Handle
+						  );
+	taskEXIT_CRITICAL();//退出临界区
+	vTaskStartScheduler();
+	
+}
+
+void StartDefaultTask(void const * argument)
+{
+  
+  for(;;)
+  {
+    osDelay(1);
+  }
+  
+}
+
+
+void EntryTask1(void const * argument)
+{
+//  uint8_t State=0;
+  while(1)
+  {
+//	HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_4);
+//	State=eTaskGetState(Task1Handle);		/*获取任务状态*/
+//	HAL_UART_Transmit(&UART_HandInit,&State,2,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//	HAL_UART_Transmit(&UART_HandInit,"\r\n",2,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+	osDelay(500);
+  }
+  
+}
+
+void EntryTask2(void const * argument)
+{
+	
+	uint8_t Count=0;
+	Count=uxTaskGetNumberOfTasks();
+	char str[50];
+	TaskStatus_t * taskstate=malloc(sizeof(TaskStatus_t));
+	uint8_t Count2=uxTaskGetSystemState(taskstate, Count,NULL);
+
+//	for(uint8_t i=0;i<Count2;i++){
+//	HAL_UART_Transmit(&UART_HandInit,taskstate[i].pcTaskName,10,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//	
+//	HAL_UART_Transmit(&UART_HandInit,"\t",1,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//		
+//	sprintf(str,"%ld",taskstate[i].uxBasePriority);
+//	HAL_UART_Transmit(&UART_HandInit,str,1,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//		
+//	HAL_UART_Transmit(&UART_HandInit,"\t",1,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//		
+//	sprintf(str,"%ld",taskstate[i].uxCurrentPriority);
+//	HAL_UART_Transmit(&UART_HandInit,str,1,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//		
+//	HAL_UART_Transmit(&UART_HandInit,"\t",1,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);	
+//	
+//	sprintf(str,"%ld",taskstate[i].xTaskNumber);
+//	HAL_UART_Transmit(&UART_HandInit,str,1,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//	
+//	HAL_UART_Transmit(&UART_HandInit,"\t",1,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//	
+//	sprintf(str,"%u",taskstate[i].usStackHighWaterMark);
+//	HAL_UART_Transmit(&UART_HandInit,str,3,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//		
+//	HAL_UART_Transmit(&UART_HandInit,"\r\n",2,100);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//	}
+
+	vTaskGetInfo(Task2Handle,taskstate,pdTRUE,eInvalid);
+	
+	HAL_UART_Transmit(&UART_HandInit,taskstate->pcTaskName,10,100);
+	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+	
+	HAL_UART_Transmit(&UART_HandInit,"\t",1,100);
+	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+	
+	sprintf(str,"%ld",taskstate->uxCurrentPriority);
+	HAL_UART_Transmit(&UART_HandInit,str,1,100);
+	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+	
+	HAL_UART_Transmit(&UART_HandInit,"\t",1,100);
+	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+	
+	sprintf(str,"%ld",taskstate->xTaskNumber);
+	HAL_UART_Transmit(&UART_HandInit,str,1,100);
+	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+	
+	HAL_UART_Transmit(&UART_HandInit,"\t",1,100);
+	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+	
+	sprintf(str,"%d",taskstate->eCurrentState);
+	HAL_UART_Transmit(&UART_HandInit,str,1,100);
+	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+	
+	HAL_UART_Transmit(&UART_HandInit,"\t",1,100);
+	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+	
+	
+	while(1)
+	{
+//		State=eTaskGetState(Task1Handle);	/*获取任务状态*/
+//		HAL_UART_Transmit(&UART_HandInit,&State,2,100);
+//		while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//		HAL_UART_Transmit(&UART_HandInit,"\r\n",2,100);
+//		while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+		osDelay(500);
+	}
+  
+}
+
+//void EntryTask3(void const * argument)
+//{
+//  
+//  while(1)
+//  {
+//	HAL_UART_Transmit(&UART_HandInit,"task3 running\r\n",15,1000);
+//	while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//	if(KEY1_Flage==1){
+//			HAL_UART_Transmit(&UART_HandInit,"Task1 Suspend\r\n",15,1000);
+//			while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//			vTaskSuspend(LEDTask1Handle);
+//			KEY1_Flage=0;
+//	}
+//	if(KEY3_Flage==1){
+//			HAL_UART_Transmit(&UART_HandInit,"Task1 Recover\r\n",15,1000);
+//			while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);
+//			vTaskResume(LEDTask1Handle);
+//			KEY3_Flage=0;
+//	}
+//	eTaskState TaskState = eTaskGetState(LEDTask1Handle);
+//	switch(TaskState){
+//		case 0: 
+//		HAL_UART_Transmit(&UART_HandInit,"Task1 eRunning\r\n",16,1000);
+//		while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);	
+//		break;
+//		case 1: 
+//		HAL_UART_Transmit(&UART_HandInit,"Task1 eReady\r\n",14,1000);
+//		while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);		
+//		break;
+//		case 2: 
+//		HAL_UART_Transmit(&UART_HandInit,"Task1 eBlocked\r\n",16,1000);
+//		while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);		
+//		break;
+//		case 3: 
+//		HAL_UART_Transmit(&UART_HandInit,"Task1 eSuspended\r\n",18,1000);
+//		while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);		
+//		break;
+//		case 4: 
+//		HAL_UART_Transmit(&UART_HandInit,"Task1 eDeleted\r\n",16,1000);
+//		while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);		
+//		break;
+//		case 5: 
+//		HAL_UART_Transmit(&UART_HandInit,"Task1 eInvalid\r\n",16,1000);
+//		while(__HAL_UART_GET_FLAG(&UART_HandInit,UART_FLAG_TC)!=1);		
+//		break;
+//	}
+//    osDelay(500);
+//  }
+//  
+//}
